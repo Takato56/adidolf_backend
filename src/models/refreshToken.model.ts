8a76fd15f64f81f45a56f46f70c6709a137cad38
Takato56 <lan.05062003@gmail.com>
2026-06-05 16:29:14 +0700

    Update refreshToken model to change userId type from String to Number

--- src/models/refreshToken.model.ts
+++ src/models/refreshToken.model.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const refreshTokenSchema = new mongoose.Schema(
     {
-        userId: { type: String, required: true },
+        userId: { type: Number, required: true },
         token: { type: String, required: true, unique: true },
         expiresAt: { type: Date, required: true }
     },
